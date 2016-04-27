c164d9f63ee4e0a63002cb85ae9e194a797aa481
Michael Peterson <mikebean233@yahoo.com>
2016-04-26 20:07:08 -0700

    Finished Lab4.

--- Lab4/app/src/main/java/michaelpeterson/cscd372/lab4/MainActivity.java
+++ Lab4/app/src/main/java/michaelpeterson/cscd372/lab4/MainActivity.java
@@ -1,15 +1,19 @@
 package michaelpeterson.cscd372.lab4;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public int[] _values;
+    public SevenSegment[] _digits;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +21,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        final MainActivity thisActivity = this;
+        final SevenSegment sevenSegment = ((SevenSegment) findViewById(R.id.sevenSegment0));
+        if(_values == null)
+            initialize();
 
+        if(savedInstanceState != null && savedInstanceState.containsKey("values")){
+            _values = savedInstanceState.getIntArray("values");
+            updateDigits();
+        }
+        Button incrButton  = ((Button) findViewById(R.id.buttonIncr));
+        incrButton.setOnClickListener(this);
+    }
+
+    private void initialize(){
+        _values = new int[]{10, 0, 1, 2, 3};
+        _digits = new SevenSegment[]{
+                (SevenSegment)findViewById(R.id.sevenSegment0),
+                (SevenSegment)findViewById(R.id.sevenSegment1),
+                (SevenSegment)findViewById(R.id.sevenSegment2),
+                (SevenSegment)findViewById(R.id.sevenSegment3),
+                (SevenSegment)findViewById(R.id.sevenSegment4)
+        };
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle bundle){
+        bundle.putIntArray("values", _values);
     }
 
     @Override
@@ -35,10 +65,25 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_about) {
+            Toast.makeText(this, "Michael Peterson Lab4", Toast.LENGTH_LONG).show();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        for(int i = 0; i < 5; ++i)
+            _values[i] = (_values[i] + 1) % 11;
+
+        updateDigits();
+    }
+
+    private void updateDigits(){
+        for(int i = 0; i < 5; ++i){
+            _digits[i].set(_values[i]);
+        }
     }
 }
